bdb1781deb69dd85648b24e23ba9da12b75c3266
Rowan Tamer <rowan.tamer@gmail.com>
2023-04-22 12:10:45 -0700

    if_C

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,10 +12,10 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
-printf("%d is positive", n);
+printf("%i is positive", n);
 else if (n == 0)
-printf("%d is zero", n);
+printf("%i is zero", n);
 else if (n < 0)
-printf("%d is negative", n);
+printf("%i is negative", n);
 return (0);
 }
