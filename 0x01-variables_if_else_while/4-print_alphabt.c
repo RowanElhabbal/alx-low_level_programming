a013bc7b0a2a7d7f6eb76d010bcf973a233db2db
Rowan Tamer <rowan.tamer@gmail.com>
2023-04-22 13:06:34 -0700

    alphabet-alpha

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void) 
+{
+char car='a';
+while (car <= 'z')
+{
+if (!(car == 'e' || car == 'q'))
+{
+putchar(car);
+}
+car++;
+}
+return (0);
+}
