7525a71e4de4139d3fa8f400e58f5d5ec3374871
Rowan Tamer <rowan.tamer@gmail.com>
2023-04-24 17:50:26 -0700

    print comb0

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,19 +9,14 @@ int main(void)
 int i = 0;
 while (i < 10)
 {
+putchar (i + 48);
 if (i != 9)
 {
-putchar (i + 48);
-putchar (44);
+putchar (',');
 putchar (' ');
+}
 i++;
 }
-
-else
-{
-putchar (i + 48);
-i++;
-}
-}
+putchar ('\n');
 return (0);
 }
