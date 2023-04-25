97dab096fc080c6e2633adf029f80e215a902ea2
Rowan Tamer <rowan.tamer@gmail.com>
2023-04-24 17:23:45 -0700

    printcombo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i = 0;
+while (i < 10)
+{
+if (i != 9)
+{
+putchar (i + 48);
+putchar (',');
+putchar (' ');
+i++;
+}
+
+else
+{
+putchar (i + 48);
+i++;
+}
+}
+putchar ('\n');
+return (0);
+}
