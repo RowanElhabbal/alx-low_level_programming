b53d1ca3579947a258e87ae9983d7405fba82294
Rowan Tamer <rowan.tamer@gmail.com>
2023-05-12 16:33:35 -0700

    arg

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ * check_num - check - string there are digit
+ * @str: array str
+ *
+ * Return: Always 0 (Success)
+ */
+int check_num(char *str)
+{
+unsigned int i;
+i = 0;
+while (i < strlen(str))
+{
+if (!isdigit(str[i]))
+{
+return (0);
+}
+i++;
+}
+return (1);
+}
