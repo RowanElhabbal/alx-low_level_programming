7ec4f73fe84923c9e44ef06cc8d7e1b9b3114289
Rowan Tamer <rowan.tamer@gmail.com>
2023-05-16 13:17:26 -0700

    one

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -8,7 +8,7 @@
  */
 char *_strdup(char *str)
 {
-char *aaa;
+char *x;
 
 int i, r = 0;
 
@@ -17,10 +17,10 @@ return (NULL);
 i = 0;
 while (str[i] != '\0')
 i++;
-aaa = malloc(sizeof(char) * (i + 1));
-if (aaa == NULL)
+x = malloc(sizeof(char) * (i + 1));
+if (x == NULL)
 return (NULL);
 for (r = 0; str[r]; r++)
-aaa[r] = str[r];
-return (aaa);
+x[r] = str[r];
+return (x);
 }
